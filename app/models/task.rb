5d156a8ff16e553e1c0023143ad439c685c1ed10
Paul Anthony McGowan <info@xhostcom.com>
2022-09-15 12:39:03 +0100

    Model methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,8 +15,13 @@ class Task < ApplicationRecord
   enum status: { no_status: 0, next_up: 1, in_progress: 2, completed: 2 }
 
   before_create :set_default_status
+  after_commit :send_notification, if: -> { status_changed? }
 
   def set_default_status
     self.status ||= :no_status
+  end
+
+  def send_notification
+    TaskMailer.with(task: self).status_changed.deliver_now
   end
 end
